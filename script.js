77a21c43566b24b2cb7115c719ec3da6ff4d258a
AbdulRahman Alam <abdulrahmanidrees@gmail.com>
2024-11-05 12:13:59 +0300

    Example 2 is  Done

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 // //Checking
 // console.log('Hello World!')
 
-//Example 1
-const maxOfTwoNumbers = (x, y) => {
-  if (x >= y) {
-    return x
+// //Example 1
+// const maxOfTwoNumbers = (x, y) => {
+//   if (x >= y) {
+//     return x
+//   } else {
+//     return y
+//   }
+// }
+
+// console.log('Exercise 1 Result:', maxOfTwoNumbers(3, 9))
+
+//Example 2
+const isAdult = (number) => {
+  if (number > 18) {
+    return 'Adult'
   } else {
-    return y
+    return 'Minor'
   }
 }
 
-console.log('Exercise 1 Result:', maxOfTwoNumbers(3, 9))
+console.log('Exercise 2 Result:', isAdult(21))
