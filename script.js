7ee09a57eb4eb390965c206a66488df2a4d030a5
AbdulRahman Alam <abdulrahmanidrees@gmail.com>
2024-11-05 12:07:01 +0300

    Example 1 is  Done

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 // //Checking
 // console.log('Hello World!')
+
+//Example 1
+const maxOfTwoNumbers = (x, y) => {
+  if (x >= y) {
+    return x
+  } else {
+    return y
+  }
+}
+
+console.log('Exercise 1 Result:', maxOfTwoNumbers(3, 9))
